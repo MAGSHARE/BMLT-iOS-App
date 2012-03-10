292e1d49aaf3df563d7d469df92052b824fdc44e
Chris M <webservant@magnaws.org>
2012-03-10 17:44:20 -0500

    Set up the headers correctly

--- dev/BMLT/BMLTSecondViewController.h
+++ dev/BMLT/BMLTSecondViewController.h
@@ -2,8 +2,20 @@
 //  BMLTSecondViewController.h
 //  BMLT
 //
-//  Created by Chris Marshall on 3/10/12.
-//  Copyright (c) 2012 Nikon Inc. All rights reserved.
+//  Created by MAGSHARE.
+//  This is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//  
+//  BMLT is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//  
+//  You should have received a copy of the GNU General Public License
+//  along with this code.  If not, see <http://www.gnu.org/licenses/>.
+
 //
 
 #import <UIKit/UIKit.h>
