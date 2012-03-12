349acb98ca6ee05fe77382caa0666d32de347f4d
Chris M <webservant@magnaws.org>
2012-03-12 14:13:33 -0400

    Moving everything I can into the storyboards.

---
+++ dev/BMLT/Main Search/BMLTAdvancedSearchViewController.m
@@ -0,0 +1,54 @@
+//
+//  BMLTAdvancedSearchViewController.m
+//  BMLT
+//
+//  Created by MAGSHARE.
+//  This is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//  
+//  BMLT is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//  
+//  You should have received a copy of the GNU General Public License
+//  along with this code.  If not, see <http://www.gnu.org/licenses/>.
+//
+
+#import "BMLTAdvancedSearchViewController.h"
+
+@interface BMLTAdvancedSearchViewController ()
+
+@end
+
+@implementation BMLTAdvancedSearchViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+@end
