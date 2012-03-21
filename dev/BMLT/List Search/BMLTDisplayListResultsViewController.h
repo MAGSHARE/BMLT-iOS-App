a678fdfab0d5a4806350b2473ed2c170e360ee87
Chris M <webservant@magnaws.org>
2012-03-21 08:11:15 -0400

    Beginning the setup for display of listed results.

---
+++ dev/BMLT/List Search/BMLTDisplayListResultsViewController.h
@@ -0,0 +1,29 @@
+//
+//  BMLTDisplayListResultsViewController.h
+//  BMLT
+//
+//  Created by MAGSHARE.
+//  This is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//  
+//  BMLT is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//  
+//  You should have received a copy of the GNU General Public License
+//  along with this code.  If not, see <http://www.gnu.org/licenses/>.
+//
+
+#import <UIKit/UIKit.h>
+#import "A_BMLTNavBarViewController.h"
+
+/**************************************************************//**
+ \class  BMLTDisplayListResultsViewController
+ \brief  This class handles display of listed search results.
+ *****************************************************************/
+@interface BMLTDisplayListResultsViewController : A_BMLTNavBarViewController <UITableViewDataSource, UITableViewDelegate>
+@property (strong, nonatomic) NSMutableArray    *dataArray;
+@end
---
+++ dev/BMLT/List Search/BMLTDisplayListResultsViewController.m
@@ -0,0 +1,65 @@
+//
+//  BMLTDisplayListResultsViewController.m
+//  BMLT
+//
+//  Created by MAGSHARE.
+//  This is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//  
+//  BMLT is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//  
+//  You should have received a copy of the GNU General Public License
+//  along with this code.  If not, see <http://www.gnu.org/licenses/>.
+//
+
+#import "BMLTDisplayListResultsViewController.h"
+
+/**************************************************************//**
+ \class  BMLTDisplayListResultsViewController - Private Interface
+ \brief  This class handles display of listed search results.
+ *****************************************************************/
+@interface BMLTDisplayListResultsViewController ()
+
+@end
+
+/**************************************************************//**
+ \class  BMLTDisplayListResultsViewController
+ \brief  This class handles display of listed search results.
+ *****************************************************************/
+@implementation BMLTDisplayListResultsViewController
+
+/**************************************************************//**
+ \brief Initializes the instance from its xib file.
+ \returns self
+ *****************************************************************/
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self)
+        {
+        }
+    return self;
+}
+
+/**************************************************************//**
+ \brief Called after the view has loaded.
+ *****************************************************************/
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+}
+
+/**************************************************************//**
+ \brief Called after the view has unloaded.
+ *****************************************************************/
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+}
+
+@end
