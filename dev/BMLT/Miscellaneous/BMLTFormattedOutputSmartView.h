d413545bc6ce0834a06c68e940eedf76367c4d42
Chris M <webservant@magnaws.org>
2012-04-12 12:20:12 -0400

    OK. I've figgered out how I will do the PDF and printing. I'll create a "smart view" class, and use that to develop the formatted output.

---
+++ dev/BMLT/Miscellaneous/BMLTFormattedOutputSmartView.h
@@ -0,0 +1,37 @@
+//
+//  BMLTFormattedOutputSmartView.h
+//  BMLT
+//
+//  Created by MAGSHARE.
+//  Copyright 2012 MAGSHARE. All rights reserved.
+//
+//  This is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//  
+//  BMLT is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//  
+//  You should have received a copy of the GNU General Public License
+//  along with this code.  If not, see <http://www.gnu.org/licenses/>.
+//
+
+#import <UIKit/UIKit.h>
+
+/**************************************************************//**
+ \class BMLTFormattedOutputSmartView
+ \brief Draws a customized version of the search results for print or PDF
+        This uses a pattern I've used a number of times: "Smart View."
+        It's an amalgam of a V and a C in the MVC pattern. Basically,
+        it's a view that can accept some model data, and is "smart"
+        enough to draw itself, based on that data. It is a great deal
+        simpler and less cumbersome to make this an atomic class,
+        as opposed to separate V and C classes. Also, it can easily
+        be repurposed and handed to different controllers for display.
+ *****************************************************************/
+@interface BMLTFormattedOutputSmartView : UIView
+- (id)initWithFrame:(CGRect)inFrame andMeetingList:(NSArray *)inMeetings;
+@end
