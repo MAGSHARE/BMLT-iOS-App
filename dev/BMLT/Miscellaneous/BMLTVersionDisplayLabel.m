23b406a12ed2f583e391457e0a77470687496569
Chris M <webservant@magnaws.org>
2012-04-19 11:25:53 -0400

    Added a version display label class.

---
+++ dev/BMLT/Miscellaneous/BMLTVersionDisplayLabel.m
@@ -0,0 +1,35 @@
+//
+//  BMLTVersionDisplayView.m
+//  BMLT
+//
+//  Created by MAGSHARE
+//  Copyright MAGSHARE. All rights reserved.
+//
+//  This is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//  
+//  BMLT is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//  
+//  You should have received a copy of the GNU General Public License
+//  along with this code.  If not, see <http://www.gnu.org/licenses/>.
+//
+
+#import "BMLTVersionDisplayLabel.h"
+
+@implementation BMLTVersionDisplayLabel
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self)
+        {
+        }
+    return self;
+}
+
+@end
