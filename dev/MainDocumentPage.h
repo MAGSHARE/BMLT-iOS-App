ccdb4983c07a22d3907ef8bb4d88d4e7c434e15f
Chris M <webservant@magnaws.org>
2012-04-20 23:32:52 -0400

    Working on a detailed main page for the docs.

--- dev/MainDocumentPage.h
+++ dev/MainDocumentPage.h
@@ -1,7 +1,10 @@
 /**
  \mainpage
- \brief <p>The BMLT iOS App is an app that is designed to work in concert with <a href="http://magshare.org/bmlt">the BMLT Server System</a>, in helping people interested in finding NA meetings.</p>
+ \brief <p>The BMLT iOS App is an app that is designed to work in concert with <a href="http://magshare.org/bmlt">the BMLT Server System</a>, in assisting those interested in finding NA meetings.</p>
  
  <p>The <a href="http://magshare.org/bmlt">BMLT</a> (which stands for <strong>B</strong>asic <strong>M</strong>eeting <strong>L</strong>ist <strong>T</strong>oolbox), is a powerful, client-server system, designed to help <a href="http://na.org">NA</a> Service bodies store, edit, and share information about their regularly-scheduled meetings.</p>
- <p>It is <a href="http://magshare.org/bmlt-users">in use in many places around the world</a>.</p>
+ 
+ <p>It is <a href="http://magshare.org/bmlt-users">in use in many places around the world</a>. The current version of this app is designed to be "tuned" for specific installations. In the future, we may be able to establish a "broker" server that could help to locate the nearest server.</p>
+
+ <div style="text-align:center"><img src="../images/BasicObjectLayout.gif" alt="Diagram of the basic object layout" style="margin-left:auto;margin-right:auto;display:block" /></div>
 */
