8d8dd6d962b8bca5293ad8ade652019d61eaf259
chris@littlegreenviper.com <webservant@magnaws.org>
2012-12-26 22:40:26 -0500

    Just corrected a couple of stale links.

--- dev/MainDocumentPage.h
+++ dev/MainDocumentPage.h
@@ -1,11 +1,11 @@
 /**
  \mainpage
  <h1 style="text-align:center">The BMLT iOS App</h1>
- <p>The BMLT iOS App is an app that is designed to work in concert with <a href="http://magshare.org/bmlt">the BMLT Server System</a>, in assisting those interested in finding NA meetings.</p>
+ <p>The BMLT iOS App is an app that is designed to work in concert with <a href="http://bmlt.magshare.net/">the BMLT Server System</a>, in assisting those interested in finding NA meetings.</p>
  
- <p>The <a href="http://magshare.org/bmlt">BMLT</a> (which stands for <strong>B</strong>asic <strong>M</strong>eeting <strong>L</strong>ist <strong>T</strong>oolbox), is a powerful, client-server system, designed to help <a href="http://na.org">NA</a> Service bodies store, edit, and share information about their regularly-scheduled meetings.</p>
+ <p>The <a href="http://bmlt.magshare.net/">BMLT</a> (which stands for <strong>B</strong>asic <strong>M</strong>eeting <strong>L</strong>ist <strong>T</strong>oolbox), is a powerful, client-server system, designed to help <a href="http://na.org">NA</a> Service bodies store, edit, and share information about their regularly-scheduled meetings.</p>
  
- <p>It is <a href="http://magshare.org/bmlt-users">in use in many places around the world</a>. The current version of this app is designed to be "tuned" for specific installations. In the future, we may be able to establish a "broker" server that could help to locate the nearest server.</p>
+ <p>It is <a href="http://bmlt.magshare.net/what-is-the-bmlt/hit-parade/">in use in many places around the world</a>. The current version of this app is designed to be "tuned" for specific installations. In the future, we may be able to establish a "broker" server that could help to locate the nearest server.</p>
 
  <div style="text-align:center">
     <h2>Basic Object Instantiation Diagram</h2>
