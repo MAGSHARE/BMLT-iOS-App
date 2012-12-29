5b687622ae826ca6a93f127550d08e0bf4ac32ff
chris@littlegreenviper.com <webservant@magnaws.org>
2012-12-28 23:13:01 -0500

    This was mostly about rearranging files. Some of the NY NA Meeting files were in the wrong place (under the AU variant), and I added the SV variant.

--- dev/Variants/BMLT-AU/BMLT_AnimationView.m
+++ dev/Variants/BMLT-AU/BMLT_AnimationView.m
@@ -43,11 +43,11 @@
     
     if ( self )
         {
-        UIImageView *bottomLayerImage = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"AU-Back.png"]];
+        UIImageView *bottomLayerImage = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"Anim-Back.png"]];
         [bottomLayerImage setFrame:[self bounds]];
         [self addSubview:bottomLayerImage];
         
-        _transparencyImage = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"AU-Transparency.png"]];
+        _transparencyImage = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"Anim-Transparency.png"]];
         [_transparencyImage setFrame:[self bounds]];
         CABasicAnimation    *fullRotation = [CABasicAnimation animationWithKeyPath:@"transform.rotation"];
         [fullRotation setFromValue:[NSNumber numberWithFloat:0]];
