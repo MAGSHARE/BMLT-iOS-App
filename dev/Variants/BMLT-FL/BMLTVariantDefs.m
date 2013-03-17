afe539367de7a0313ac0611b1455a03f0507547e
chris@littlegreenviper.com <webservant@magnaws.org>
2013-03-16 22:55:39 -0400

    Added a Florida variant, and fixed some mislocated files.

---
+++ dev/Variants/BMLT-FL/BMLTVariantDefs.m
@@ -0,0 +1,176 @@
+//
+//  BMLTVariantDefs.m
+//  BMLT
+//
+//  Created by MAGSHARE.
+//  This is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  BMLT is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this code.  If not, see <http://www.gnu.org/licenses/>.
+//
+
+#import "BMLTVariantDefs.h"
+#import "BMLTAppDelegate.h"
+
+/*****************************************************************/
+/**
+ \brief  See the "BMLTVariantDefs.h" file for details.
+ *****************************************************************/
+@implementation BMLTVariantDefs
+
+/*****************************************************************/
++ (NSString *)distanceUnits
+{
+    NSString    *plistPath = [[NSBundle mainBundle] pathForResource:@"Info" ofType:@"plist"];
+    
+    return [[NSDictionary dictionaryWithContentsOfFile:plistPath] valueForKey:@"BMLTDistanceUnits"];
+}
+
+/*****************************************************************/
++ (float)initialMapProjection
+{
+    NSString    *plistPath = [[NSBundle mainBundle] pathForResource:@"Info" ofType:@"plist"];
+    
+    return [[[NSDictionary dictionaryWithContentsOfFile:plistPath] valueForKey:@"BMLTInitialProjectionSizeInKM"] floatValue];
+}
+
+/*****************************************************************/
++ (CLLocationCoordinate2D)mapDefaultCenter;
+{
+    NSString    *plistPath = [[NSBundle mainBundle] pathForResource:@"Info" ofType:@"plist"];
+    float       longitude = [[[NSDictionary dictionaryWithContentsOfFile:plistPath] valueForKey:@"BMLTServerHomeLong"] floatValue];
+    float       latitude = [[[NSDictionary dictionaryWithContentsOfFile:plistPath] valueForKey:@"BMLTServerHomeLat"] floatValue];
+    
+    return CLLocationCoordinate2DMake(latitude, longitude);
+}
+
+/*****************************************************************/
++ (UIColor *)windowBackgroundColor
+{
+    return [UIColor colorWithRed:0.1098039216 green:0.1019607843 blue:0.7490196078 alpha:1.0];
+}
+
+/*****************************************************************/
++ (UIColor *)searchBackgroundColor
+{
+    return [[self class] windowBackgroundColor];
+}
+
+/*****************************************************************/
++ (UIColor *)listResultsBackgroundColor
+{
+    return [[self class] windowBackgroundColor];
+}
+
+/*****************************************************************/
++ (UIColor *)multiMeetingsBackgroundColor
+{
+    return [UIColor clearColor];
+}
+
+/*****************************************************************/
++ (UIColor *)multiMeetingsTextColor
+{
+    return [UIColor whiteColor];
+}
+
+/*****************************************************************/
++ (UIColor *)mapResultsBackgroundColor
+{
+    return [[self class] windowBackgroundColor];
+}
+
+/*****************************************************************/
++ (UIColor *)meetingDetailBackgroundColor
+{
+    return [UIColor colorWithRed:0.01 green:0.2 blue:0.3 alpha:1.0];
+}
+
+/*****************************************************************/
++ (UIColor *)modalBackgroundColor
+{
+    return [UIColor colorWithRed:0.1 green:0.2 blue:0.4 alpha:1.0];
+}
+
+/*****************************************************************/
++ (UIColor *)popoverBackgroundColor
+{
+    return [[self class] modalBackgroundColor];
+}
+
+/*****************************************************************/
++ (UIColor *)settingsBackgroundColor
+{
+    return [[self class] modalBackgroundColor];
+}
+
+/*****************************************************************/
++ (UIColor *)infoBackgroundColor
+{
+    return [[self class] windowBackgroundColor];
+}
+
+/*****************************************************************/
++ (UIColor *)getSortOddColor
+{
+    return [UIColor whiteColor];
+}
+
+/*****************************************************************/
++ (UIColor *)getSortEvenColor
+{
+    return [UIColor colorWithRed:0.8 green:0.9 blue:1.0 alpha:1.0];
+}
+
+/*****************************************************************/
++ (CGSize)pdfPageSize
+{
+    return CGSizeMake(612, 792);
+}
+
+/*****************************************************************/
++ (NSString *)pdfTempFileNameFormat
+{
+    return @"BMLTPDFTemp_%d.pdf";
+}
+
+/*****************************************************************/
++ (NSURL *)rootServerURI
+{
+    NSString    *plistPath = [[NSBundle mainBundle] pathForResource:@"Info" ofType:@"plist"];
+    NSString    *uriString = [[NSDictionary dictionaryWithContentsOfFile:plistPath] valueForKey:@"BMLTRootServerURI"];
+    return [NSURL URLWithString:uriString];
+}
+
+/*****************************************************************/
++ (NSURL *)directionsURITo:(CLLocationCoordinate2D)inTo   ///< The long/lat of wehere we are going
+{
+    NSURL       *ret = nil;
+    CLLocation  *lastLoc = [[BMLTAppDelegate getBMLTAppDelegate] lastLocation];
+    
+    if ( lastLoc )  // If we know where we are, then we add that to the URI, for a richer user experience.
+        {
+        ret = [NSURL URLWithString:[NSString stringWithFormat:NSLocalizedString(@"DIRECTIONS-URI-FORMAT-ACCURATE",nil), lastLoc.coordinate.latitude, lastLoc.coordinate.longitude, inTo.latitude, inTo.longitude]];
+        }
+    else    // Otherwise, we let the user figger it out when they get to the page.
+        {
+        ret = [NSURL URLWithString:[NSString stringWithFormat:NSLocalizedString(@"DIRECTIONS-URI-FORMAT",nil), inTo.latitude, inTo.longitude]];
+        }
+    
+    return ret;
+}
+
+/*****************************************************************/
++ (NSInteger)maxNumberOgMeetings
+{
+    return 500;
+}
+@end
---
+++ dev/Variants/BMLT-FL/BMLT_AnimationView.m
@@ -0,0 +1,82 @@
+//
+//  BMLT_AnimationView.m
+//  BMLT
+//
+//  Created by MAGSHARE on 8/13/11.
+//  Copyright 2011 MAGSHARE. All rights reserved.
+//
+//  This is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//  
+//  BMLT is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//  
+//  You should have received a copy of the GNU General Public License
+//  along with this code.  If not, see <http://www.gnu.org/licenses/>.
+//
+
+#import "BMLT_AnimationView.h"
+#import <QuartzCore/QuartzCore.h>
+
+// Conversion from degrees to radians.
+#define DEGREES_TO_RADIANS(angle) (angle / 180.0 * M_PI)
+
+@interface BMLT_AnimationView ()
+{
+    UIImageView *_transparencyImage;
+}
+@end;
+
+@implementation BMLT_AnimationView
+/***************************************************************\**
+ \brief This simply sets a very simple animation, in which a "radar" screen
+        is displayed over a map of Australia.
+ \returns self
+ *****************************************************************/
+- (id)initWithCoder:(NSCoder *)aDecoder
+{
+    self = [super initWithCoder:aDecoder];
+    
+    if ( self )
+        {
+        UIImageView *bottomLayerImage = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"Anim-Back.png"]];
+        [bottomLayerImage setFrame:CGRectInset([self bounds], 1, 1)];
+        [self addSubview:bottomLayerImage];
+        
+        _transparencyImage = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"Anim-Transparency.png"]];
+        [_transparencyImage setFrame:[self bounds]];
+        CABasicAnimation    *fullRotation = [CABasicAnimation animationWithKeyPath:@"transform.rotation"];
+        [fullRotation setFromValue:[NSNumber numberWithFloat:0]];
+        [fullRotation setToValue:[NSNumber numberWithFloat:((360*M_PI)/180)]];
+        [fullRotation setDuration:3.5f];
+        [fullRotation setRepeatCount:MAXFLOAT];
+        
+        [[_transparencyImage layer] addAnimation:fullRotation
+                                          forKey:@"360"];
+        
+        [self addSubview:_transparencyImage];
+        }
+    
+    return self;
+}
+
+/***************************************************************\**
+ \brief This starts the animation.
+ *****************************************************************/
+- (void)startAnimating
+{
+    [_transparencyImage startAnimating];
+}
+
+/***************************************************************\**
+ \brief This stops the animation.
+ *****************************************************************/
+- (void)stopAnimating
+{
+    [_transparencyImage stopAnimating];
+}
+@end
